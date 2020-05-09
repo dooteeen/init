39687b5111e57bd0013e8c745c8456d2e7bf8916
rkbk60 <b8csmlplg@gmail.com>
2020-05-09 09:01:36 +0900

    typo: completions @ fish

--- fish.sh
+++ fish.sh
@@ -1,6 +1,6 @@
 post_hook_fish() {
     mkdir -p $HOME/.config/fish/functions
-    mkdir -p $HOME/.config/fish/complitions
+    mkdir -p $HOME/.config/fish/completions
     ln -sf $DOTFILES/fish/config.fish $HOME/.config/fish/config.fish
     ln -sf $DOTFILES/fish/fishfile    $HOME/.config/fishfile
     echo "#!$(which fish)" $HOME/.fish_local
