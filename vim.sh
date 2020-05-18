3e4b16857513cde71bb4ab48e596c09aac38ecdc
rkbk60 <b8csmlplg@gmail.com>
2020-05-18 21:28:53 +0900

    update: uninstall vim-tiny on Debian family

--- vim.sh
+++ vim.sh
@@ -1,3 +1,13 @@
+pre_hook_vim() {
+    check_wheel || return 0
+    case "$(detect_os_base)":
+        "debian")
+            dpkg -s vim-tiny >/dev/null 2>&1 \
+                && $(append_sudo) apt remove -y vim-tiny
+            ;;
+    esac
+}
+
 post_hook_vim() {
     grep -q "source $DOTFILES/vim/vimrc" $HOME/.vimrc && return 0
     echo "source $DOTFILES/vim/vimrc"  >> $HOME/.vimrc
